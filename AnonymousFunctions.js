577d12fc5a7e56846184f7f21f420a56a3ff1c2d
Clayton <clayton.schroeder@alumni.ubc.ca>
2016-11-03 21:13:53 +0000

    modified with map function

--- AnonymousFunctions.js
+++ AnonymousFunctions.js
@@ -3,7 +3,13 @@ var input = [
   { x: 12, y: 5 },
   { x: 8, y: 15 }
 ];
-var result = input.map(/* Your code here */);
+
+
+var result = input.map(function(values) {
+var xySquared = Math.pow(values.x, 2) + Math.pow(values.y, 2);
+var result = Math.sqrt(xySquared);
+return result;
+});
 
 console.log(result[0] === 5);
 console.log(result[1] === 13);
