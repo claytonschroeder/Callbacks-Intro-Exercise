ee9a685c81a36d9e0a22479d0f0a448521f8cb49
Clayton <clayton.schroeder@alumni.ubc.ca>
2016-11-04 00:41:49 +0000

    Added anonymous function with callback

---
+++ CallbackMap.js
@@ -0,0 +1,16 @@
+var newArray = [];
+
+function map(array, elm) {
+  array.forEach(function(element) {
+    newArray.push(elm
+  (element));
+  });
+
+  console.log(newArray);
+}
+  function elm(index) {
+    return index.length;
+  }
+
+  map(["whats", "up", "my", "dudes"], elm
+);
